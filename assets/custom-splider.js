aa08bd5d0bd71ba9cb7429b18a5d6d10d311858a
unknown <venkateshgolla134@gmail.com>
2024-04-24 19:29:11 +0530

    all

--- assets/custom-splider.js
+++ assets/custom-splider.js
@@ -1,18 +1,25 @@
-if (!customElements.get("splider-component")) {
-class CustomSlider extends HTMLElement {
-    constructor() {
-      super();
-      this.element = this;
-      console.log(this.element)
-      this.options = JSON.parse(this.element.dataset.sliderOptions);
-      console.log(this.options)
-      this.mountSplider()
-    }
+if(!customElements.get("splider-component")) {
+  class SpliderComponent extends HTMLElement{
   
-    mountSplider() {
-      new Splide(this.element,this.options).mount();
+     constructor(){
+        super();
+        this.element = this
+        console.log(this)
+        this.options = JSON.parse(this.element.dataset.sliderOptions);
+        console.log(this.options);
+  
+        this.displaySliderShow()
+  
+     }
+  
+      displaySliderShow(){
+  
+        new Splide(this.element, this.options).mount()
+      }
+  
+  
+  
     }
+    customElements.define("splider-component", SpliderComponent)
+  
   }
-  
-  customElements.define("splider-component", CustomSlider);
-}
