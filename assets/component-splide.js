d69a35d050ceee266d6cdc7954d6d3389adaf9c4
unknown <venkateshgolla134@gmail.com>
2024-04-24 08:54:20 +0530

    all

---
+++ assets/component-splide.js
@@ -0,0 +1,34 @@
+if(!customElements.get("custom-slider")) {
+class CustomSlider extends HTMLElement{
+
+   constructor(){
+      super();
+      this.element = this
+      console.log(this)
+      this.options = JSON.parse(this.element.dataset.sliderOptions);
+      console.log(this.options);
+
+      this.displaySliderShow()
+
+   }
+
+    displaySliderShow(){
+
+      this.reviews =    new Splide(this.element, this.options).mount()
+      this.imageSlide = new Splide("#display-image", {
+         type: "fade",
+         perPage:1,
+         pagination:false,
+         arrows: false
+      }).mount()
+      this.reviews.sync(this.imageSlide)
+    }
+
+
+
+  }
+  customElements.define("custom-slider", CustomSlider)
+
+}
+
+  
---
+++ assets/our-story-section.js
@@ -0,0 +1,7 @@
+  document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide('#ourStorySplider', {
+        autoplay: false,
+        pagination: true,
+        arrows: false
+    }).mount();
+  });
